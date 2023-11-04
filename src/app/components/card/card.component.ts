4635852c0a4252b69e2ad90eb0d4b6129eafa223
Mah Tristao <marjietristao@gmail.com>
2023-11-05 00:16:39 +0100

    created cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string = ""
+	@Input()
+	gamePrice:string = ""
+	constructor() { }
 
   ngOnInit(): void {
   }
